9229a23ea5130efa9e94def006c577f9066c2c49
MattIPv4 <matthew@cowley.org.uk>
2019-04-11 10:59:38 +0100

    add references and encrypt emails

---
+++ assets/js/atob.js
@@ -0,0 +1,24 @@
+/**
+ *  Matt Cowley - CV: An elegant and expandable CV design made with SASS, HTML and love.
+ *  <https://github.com/MattIPv4/CV/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/MattIPv4/CV/blob/master/LICENSE.md> or <http://www.gnu.org/licenses/>.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    var atobs = document.querySelectorAll("[data-atob]");
+    for (var i = 0; i < atobs.length; ++i) {
+        atobs[i].innerHTML = atob(atobs[i].innerHTML);
+        atobs[i].removeAttribute("data-atob");
+    }
+});
